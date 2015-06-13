a1a3aaa4480aff1f35d06f6c543dfb53c6b810f0
Matt Chadburn <matt.chadburn@ft.com>
2015-06-13 09:55:06 +0100

    comments

--- promise-3.js
+++ promise-3.js
@@ -1,4 +1,4 @@
-// $ npm install rx randomstring es6-promise
+// `npm install rx randomstring es6-promise`
 
 var Rx				= require('rx');
 var randomstring	= require("randomstring");
@@ -15,33 +15,32 @@ setInterval(function () {
 	}));
 }, 500);
 
-
 var enrichmentStream = sqsStream
-	.map(function (data) {
+	.map(function (data) {			// -- 1. Transform the data in to a nice format
 		return JSON.parse(data);
 	})
-	.flatMap(function (data) {	// async
+	.flatMap(function (data) {		// -- 2. Example async operation
 		return Promise
 			.all([
 				new Promise(function(resolve, reject) { // a response from CAPI
 					resolve(1);
 				}), 
 				new Promise(function(resolve, reject) { // a response from Session API
-					resolve(2);
+					resolve(2);	// TODO - randomly delay and fail this
 				}),
 				Promise.resolve(data)	// not sure how to return the original data other than this
 			])
 	})
-	.map(function (data) {
+	.map(function (data) {			// -- 3. Append the async results to the original data structure
 		data[2].annotations = { a: data[0], b: data[1] };	// TODO destructure
 		return data[2];
 	})
-	.map(function (data) {
+	.map(function (data) {			// -- 4. Some token transform on a property
 		data.annotations.foo = data.session.replace(/[a-z]/g, '-'); 
 		return data;
 	})
-	.flatMap(function (data) {	// example of how we might conditionally do something asynchronously
-		if (/^[0-9]/.test(data.session)) {// some conditional
+	.flatMap(function (data) {		// -- 5. Example of how we might conditionally do something asynchronously, downstream of the above transforms
+		if (/^[0-9]/.test(data.session)) {
 			return Promise
 				.all([
 					Promise.resolve(data),	// the original data structure must always be passed first 
@@ -50,10 +49,10 @@ var enrichmentStream = sqsStream
 					}) 
 				])
 		} else {
-			return Promise.all([Promise.resolve(data)])	// pass through
+			return Promise.all([Promise.resolve(data)])	// pass through if conditional is false
 		}
 	})
-	.map(function (data) { // feels a bit unnatural
+	.map(function (data) { 			// -- 6. Conditionally append the previous result 
 		if (data[1]) data[0].annotations.boo = data[1];
 		return data[0];
 	})
@@ -62,7 +61,7 @@ var enrichmentStream = sqsStream
 // output
 enrichmentStream.subscribe(
     function (x) {
-		console.log('Next', JSON.stringify(x))
+		console.log('Next', JSON.stringify(x))	// Eg. push to kinesis
     },
     function (err) {
         console.log('Error: ' + err);
@@ -71,16 +70,3 @@ enrichmentStream.subscribe(
         console.log('Completed');
     }
 )
-
-enrichmentStream.subscribe(
-    function (x) {
-		console.log('Next', JSON.stringify(x))
-    },
-    function (err) {
-        console.log('Error: ' + err);
-    },
-    function () {
-        console.log('Completed');
-    }
-)
-
