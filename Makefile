0c906bcc4fec6098770454a7832a4616387b6902
Matt Chadburn <matt.chadburn@ft.com>
2015-06-13 13:38:02 +0100

    es

--- Makefile
+++ Makefile
@@ -1,7 +1,3 @@
-HEROKU_AUTH_TOKEN := `heroku auth:token`
 
 run:
-	@export accessKey=`cat ~/.aws-access.spoor`; \
-	 export secretAccessKey=`cat ~/.aws-secret.spoor`; \
-	 export SQS_EGEST=`cat ~/.aws-sqs.spoor-egest`; \
-	 node sqs.js
+	@gulp compile; node dist/promise-3.js
--- src/promise-3.js
+++ src/promise-3.js
@@ -2,49 +2,54 @@
 
 var Rx				= require('rx');
 var randomstring	= require("randomstring");
+var transforms		= require('./transforms');
+
 require('es6-promise').polyfill();
 
 // -------- Simulated SQS stream
 
 var sqsStream = new Rx.Subject();
 
-setInterval(function () {
+setInterval(() => {
 	console.log('SQS generated an event');
 	sqsStream.onNext(JSON.stringify({
-		session: randomstring.generate(),
+		session: randomstring.generate()
 	}));
 }, 500);
 
+// -------- Enrichment pipeline 
+
 var enrichmentStream = sqsStream
-	.map(function (data) {			// -- 1. Transform the data in to a nice format
+	.map(data => {			// -- 1. Transform the data in to a nice format
 		return JSON.parse(data);
 	})
-	.flatMap(function (data) {		// -- 2. Example async operation
+	.flatMap(data => {		// -- 2. Example async operation
 		return Promise
 			.all([
-				new Promise(function(resolve, reject) { // a response from CAPI
+				new Promise((resolve, reject) => { // a response from CAPI
 					resolve(1);
 				}), 
-				new Promise(function(resolve, reject) { // a response from Session API
+				new Promise((resolve, reject) => { // a response from Session API
 					resolve(2);	// TODO - randomly delay and fail this
 				}),
 				Promise.resolve(data)	// not sure how to return the original data other than this
 			])
 	})
-	.map(function (data) {			// -- 3. Append the async results to the original data structure
-		data[2].annotations = { a: data[0], b: data[1] };	// TODO destructure
+	.map(data => {			// -- 3. Append the async results to the original data structure
+		var [a, b] = data;
+		data[2].annotations = { a: a, b: b };
 		return data[2];
 	})
-	.map(function (data) {			// -- 4. Some token transform on a property
+	.map(data => {			// -- 4. Some token transform on a property
 		data.annotations.foo = data.session.replace(/[a-z]/g, '-'); 
 		return data;
 	})
-	.flatMap(function (data) {		// -- 5. Example of how we might conditionally do something asynchronously, downstream of the above transforms
+	.flatMap(data => {		// -- 5. Example of how we might conditionally do something asynchronously, downstream of the above transforms
 		if (/^[0-9]/.test(data.session)) {
 			return Promise
 				.all([
 					Promise.resolve(data),	// the original data structure must always be passed first 
-					new Promise(function(resolve, reject) { // a response from some async process
+					new Promise((resolve, reject) => { // a response from some async process
 						resolve(3);
 					}) 
 				])
@@ -52,7 +57,7 @@ var enrichmentStream = sqsStream
 			return Promise.all([Promise.resolve(data)])	// pass through if conditional is false
 		}
 	})
-	.map(function (data) { 			// -- 6. Conditionally append the previous result 
+	.map(data => { 			// -- 6. Conditionally append the previous result 
 		if (data[1]) data[0].annotations.boo = data[1];
 		return data[0];
 	})
@@ -60,13 +65,13 @@ var enrichmentStream = sqsStream
 
 // output
 enrichmentStream.subscribe(
-    function (x) {
+    x => {
 		console.log('Next', JSON.stringify(x))	// Eg. push to kinesis
     },
-    function (err) {
+    err => {
         console.log('Error: ' + err);
     },
-    function () {
+    () => {
         console.log('Completed');
     }
 )
